20105ead1bd57823bf56cd5c3ba6846469a6e858
patrickmoehrke7 <patrickmoehrke@dupro.co.uk>
2023-01-20 10:00:05 +0200

    Chore: add author, license, and echos

--- create-layer.sh
+++ create-layer.sh
@@ -4,16 +4,20 @@
 # License: MIT License
 
 ## 1. Create the Docker image containing the Python packages based off the Dockerfile
+echo "Building Docker image..."
 docker build -t python-3-8-docker .
 
 ## 2. Run the docker container as a background process
+echo "Running container in background..."
 docker create python-3-8-docker
 
 ## 3. Get the name of the Docker container
 NAME=$(docker ps -l -q)
 
 ## 4. Copy the zipped package from the Docker container into output-layer/ 
+echo "Copying the zip file from the container into output-layer/..."
 docker cp $NAME:python-layer3-8.zip output-layer/
 
 ## 5. Delete the image and clean up
+echo "Deleting image and cleaning up..."
 docker image rm -f python-3-8-docker
