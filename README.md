7029a30241b6979c23e72b30be1f352604b3a593
patrickm663 <rev0cation2025@outlook.com>
2024-06-14 21:35:47 +0200

    Refactor: redo how temporary Dockerfiles and images are created and support newer Python versions

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Utility for Creating Custom Python Layers for AWS Lambda
-This is a simple utility for adding user-defined packages to a Python 3.7-3.9 AWS Lambda layer. The script spins up a Python3.X-slim Docker image to install and zip the packages you need.
+This is a simple utility for adding user-defined packages to a Python 3.8-3.12 (latest at time of writing) AWS Lambda layer. The script spins up a Python3.X-slim Docker image to install and zip the packages you need.
 
 Please note existing limitations of AWS Lambda such as the 250MB layer limit.
 
 ## Prerequisites
 - A UNIX environment to run the shell script
 - Docker installed
+- Since `sed` is used to find-and-replace the users's selected Python version, results may vary on Windows :) (testing on Windows is in works).
 
 ## Usage
 1.  Clone the repository:
@@ -14,13 +15,13 @@ git clone https://github.com/patrickm663/aws-lambda-layer-generator.git
 ```
 2.  Enter the directory:
 ```
-cd aws-lambda-layer-generator/
+cd aws-lambda-layer-generator
 ```
-3. Update the `requirements.txt` to include the packages you wish to include in your layer:
+3. Update the `requirements.txt` file in `src` to indicate the packages you wish to include in your layer (like you would an ordinary `requirements.txt` file):
 ```
 vim src/requirements.txt
 ```
-4. Set the shell script `create-layer.sh` as executable:
+4. (Optional) Set the shell script `create-layer.sh` as executable:
 ```
 chmod +x create-layer.sh
 ```
@@ -34,6 +35,9 @@ chmod +x create-layer.sh
 _Note: the current commands are set to **overwrite** `python-layer3-X.zip` each time the script is run._
 
 8. Add the zip file as a layer in AWS Lambda, using S3 where needed.
+
+## Hacking
+Since this is just a Bash script doing find and-replace on a central Dockerfile (`Dockerfile_master`), users can edit the script as they please. This also means users can find-and-replace manually if they only want to use a fixed Python distribution (say, Python 3.12). The rest of the commands to build the container, copy the layer, and clean-up can be done manually or embedded as part of a larger automation piece.
 
 ## Contributing
 Pull requests are welcome and encouraged!
--- create-layer.sh
+++ create-layer.sh
@@ -5,75 +5,60 @@
 
 echo "Python AWS Layer Generator"
 echo " "
-echo "1 - Python 3.7"
-echo "2 - Python 3.8"
-echo "3 - Python 3.9"
+echo "1 - Python 3.8"
+echo "2 - Python 3.9"
+echo "3 - Python 3.10"
+echo "4 - Python 3.11"
+echo "5 - Python 3.12"
 echo "Select a Python version: "
+
 read version;
 
-mkdir -p output-layer/
-
 case $version in
-    1) 
-        ## 1. Create the Docker image containing the Python packages based off the Dockerfile
-        echo "Building Docker image for Python 3.7..."
-        docker build -t python-3-7-docker -f Dockerfile3-7 .
-
-        ## 2. Run the docker container as a background process
-        echo "Running container in background..."
-        docker create python-3-7-docker
-
-        ## 3. Get the name of the Docker container
-        NAME=$(docker ps -l -q)
-
-        ## 4. Copy the zipped package from the Docker container into output-layer/ 
-        echo "Copying the zip file from the container into output-layer/..."
-        docker cp $NAME:python-layer3-7.zip output-layer/
-
-        ## 5. Delete the image and clean up
-        echo "Deleting image and cleaning up..."
-        docker image rm -f python-3-7-docker
-        ;;
-    2) 
-        ## 1. Create the Docker image containing the Python packages based off the Dockerfile
-        echo "Building Docker image for Python 3.8..."
-        docker build -t python-3-8-docker -f Dockerfile3-8 .
-
-        ## 2. Run the docker container as a background process
-        echo "Running container in background..."
-        docker create python-3-8-docker
-
-        ## 3. Get the name of the Docker container
-        NAME=$(docker ps -l -q)
-
-        ## 4. Copy the zipped package from the Docker container into output-layer/ 
-        echo "Copying the zip file from the container into output-layer/..."
-        docker cp $NAME:python-layer3-8.zip output-layer/
-
-        ## 5. Delete the image and clean up
-        echo "Deleting image and cleaning up..."
-        docker image rm -f python-3-8-docker
-        ;;
-    3) 
-        ## 1. Create the Docker image containing the Python packages based off the Dockerfile
-        echo "Building Docker image for Python 3.9..."
-        docker build -t python-3-9-docker -f Dockerfile3-9 .
-
-        ## 2. Run the docker container as a background process
-        echo "Running container in background..."
-        docker create python-3-9-docker
-
-        ## 3. Get the name of the Docker container
-        NAME=$(docker ps -l -q)
-
-        ## 4. Copy the zipped package from the Docker container into output-layer/ 
-        echo "Copying the zip file from the container into output-layer/..."
-        docker cp $NAME:python-layer3-9.zip output-layer/
-
-        ## 5. Delete the image and clean up
-        echo "Deleting image and cleaning up..."
-        docker image rm -f python-3-9-docker
-        ;;
-    *)
-        echo "Invalid option $version";;
+  1) 
+    PYTHON_VERSION="8"
+    ;;
+  2) 
+    PYTHON_VERSION="9"
+    ;;
+  3)
+    PYTHON_VERSION="10"
+    ;;
+  4)
+    PYTHON_VERSION="11"
+    ;;
+  5)
+    PYTHON_VERSION="12"
+    ;;
+  *)
+    echo "Invalid option $version"
+    echo "Exiting"
+    exit 1
+    ;;
 esac
+
+
+## 0. Update the Dockerfile master version based on the user choice
+sed "s/3\.7/3\.$PYTHON_VERSION/" Dockerfile_master > Dockerfile_temp_
+sed "s/3\-7/3\-$PYTHON_VERSION/" Dockerfile_temp_ > Dockerfile_temp
+rm Dockerfile_temp_
+
+## 1. Create the Docker image containing the Python packages based off the Dockerfile
+echo "Building Docker image for Python 3.$PYTHON_VERSION"
+docker build -t python-3-docker -f Dockerfile_temp .
+
+## 2. Run the docker container as a background process
+echo "Running container in background..."
+docker create python-3-docker
+
+## 3. Get the name of the Docker container
+NAME=$(docker ps -l -q)
+
+## 4. Copy the zipped package from the Docker container into output-layer/ 
+echo "Copying the zip file from the container into output-layer/..."
+docker cp $NAME:python-layer3-$PYTHON_VERSION.zip output-layer/
+
+## 5. Delete the image and clean up
+echo "Deleting image and cleaning up..."
+docker image rm -f python-3-docker
+rm Dockerfile_temp
